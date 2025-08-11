0759d167bcf84ccc2a17a2c98f240eb2b7541488
elimxim <elimxim@gmail.com>
2025-08-11 15:56:36 +0200

    lilrocket

--- src/main/vue/src/views/auth/LoginView.vue
+++ src/main/vue/src/views/auth/LoginView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="auth-container">
+  <div class="rocket-container">
+    <img
+      ref="lilrocket"
+      class="lilrocket"
+      alt="Lilrocket" />
+  </div>
+  <div class="auth-container" style="margin-top: 0;">
     <form @submit.prevent="handleLogin">
       <input v-model="email" type="email" placeholder="Email" required/>
       <!-- todo ability to see the password -->
@@ -11,8 +17,7 @@
 
 <script setup lang="ts">
 import '@/assets/auth.css'
-import apiClient from '@/api/api-client.ts'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from "@/stores/auth-store.ts"
 import { routeNames } from "@/router/index.js"
@@ -34,7 +39,97 @@ async function handleLogin() {
     console.log('Failed to log in: ', error)
   }
 }
+
+// lilrocket>
+
+import clayImg from '@/assets/rocket/clay.png'
+import crochetImg from '@/assets/rocket/crochet.png'
+import originalImg from '@/assets/rocket/original.png'
+import toyImg from '@/assets/rocket/toy.png'
+
+const rockets = [
+  clayImg,
+  crochetImg,
+  originalImg,
+  clayImg,
+  toyImg,
+  crochetImg
+]
+
+const lilrocket = ref<HTMLImageElement>()
+
+function setRandomLilrocket(){
+  if (lilrocket.value) {
+    const idx = Math.floor(Math.random() * rockets.length)
+    lilrocket.value.src = rockets[idx]
+  }
+}
+
+onMounted(() => {
+  setRandomLilrocket()
+})
+
+// <lilrocket
+
 </script>
 
 <style scoped>
+.rocket-container {
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+}
+
+.lilrocket {
+  height: 8em;
+  margin-top: 5rem;
+  animation:
+    bounce-in 1s,
+    shake 4s infinite 1s;
+  cursor: pointer;
+  user-select: none;
+  /* Chrome, Safari */
+  -webkit-user-drag: none;
+  /* Mozilla Firefox */
+  -moz-user-drag: none;
+  /* Microsoft Edge, Internet Explorer */
+  -ms-user-drag: none;
+  /* Standard property */
+  user-drag: none;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.lilrocket:hover {
+  animation: shake 1s infinite;
+}
+
+@keyframes shake {
+  0% { transform: translate(2px, 3px) rotate(1deg); }
+  10% { transform: translate(-3px, -5px) rotate(-3deg); }
+  20% { transform: translate(-5px, 2px) rotate(2deg); }
+  30% { transform: translate(5px, 4px) rotate(-1deg); }
+  40% { transform: translate(3px, -3px) rotate(3deg); }
+  50% { transform: translate(-3px, 5px) rotate(-2deg); }
+  60% { transform: translate(-5px, 3px) rotate(1deg); }
+  70% { transform: translate(5px, 3px) rotate(-3deg); }
+  80% { transform: translate(-3px, -3px) rotate(2deg); }
+  90% { transform: translate(3px, 5px) rotate(-1deg); }
+  100% { transform: translate(2px, 3px) rotate(1deg); }
+}
+
+.lilrocket:active {
+  animation: shake 0.3s infinite;
+
+}
 </style>
