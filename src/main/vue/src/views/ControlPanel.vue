1700d46f0a8bc56cd29ef716a79d58f9e7943d53
elimxim <elimxim@gmail.com>
2025-10-15 14:28:11 +0400

    ControlPanel changes

--- src/main/vue/src/views/ControlPanel.vue
+++ src/main/vue/src/views/ControlPanel.vue
@@ -20,6 +20,41 @@
           />
         </div>
         <div class="launch-panel">
+          <div class="calendar-panel">
+            <div class="calendar-panel__main">
+              <AwesomeButton
+                icon="fa-solid fa-calendar-days"
+                class="calendar-panel__main__button"
+                :disabled="!flashcardSet"
+                :on-click="modalStore.toggleCalendar"
+              />
+              <div class="calendar-panel__main__day">
+                <div class="calendar-panel__main__day__text">
+                  Day
+                </div>
+                <div v-if="isCurrDayOff" class="calendar-panel__main__day__vacation">
+                  🌴
+                </div>
+                <div v-else class="calendar-panel__main__day__number">
+                  400
+                </div>
+              </div>
+            </div>
+            <div class="calendar-panel__stages">
+              <div class="calendar-stage">
+                <div class="calendar-stage__label">Stage 2</div>
+                <div class="calendar-stage__value">20</div>
+              </div>
+              <div class="calendar-stage">
+                <div class="calendar-stage__label">Stage 1</div>
+                <div class="calendar-stage__value">200</div>
+              </div>
+              <div class="calendar-stage">
+                <div class="calendar-stage__label">Total</div>
+                <div class="calendar-stage__value">220</div>
+              </div>
+            </div>
+          </div>
           <LaunchButton
             label="Start Review"
             :on-click="onLaunch"
@@ -30,6 +65,9 @@
     </div>
   </div>
   <FlashcardSetSettingsModal/>
+  <FlashcardSetCreationModal/>
+  <FlashcardModificationModal/>
+  <CalendarModal/>
 </template>
 
 <script setup lang="ts">
@@ -37,21 +75,38 @@ import FlashcardSetSideBar from '@/components/control-panel/FlashcardSetSideBar.
 import AwesomeButton from '@/components/AwesomeButton.vue'
 import LaunchButton from '@/components/control-panel/LaunchButton.vue'
 import FlashcardSetSettingsModal from '@/views/modal/FlashcardSetSettingsModal.vue'
+import FlashcardSetCreationModal from '@/views/modal/FlashcardSetCreationModal.vue'
+import FlashcardModificationModal from '@/views/modal/FlashcardModificationModal.vue'
+import CalendarModal from '@/views/modal/CalendarModal.vue'
 import { useFlashcardStore } from '@/stores/flashcard-store.ts'
 import { useModalStore } from '@/stores/modal-store.ts'
 import { storeToRefs } from 'pinia'
 import { computed, onMounted, ref } from 'vue'
 import { loadFlashcardSetStore } from '@/shared/stores.ts'
+import { useChronoStore } from '@/stores/chrono-store.ts'
+import { chronodayStatuses } from '@/core-logic/chrono-logic.ts';
 
 const flashcardStore = useFlashcardStore()
+const chronoStore = useChronoStore()
 const modalStore = useModalStore()
 
 const { flashcardSet } = storeToRefs(flashcardStore)
+const { currDay } = storeToRefs(chronoStore)
 
 const sidebar = ref<InstanceType<typeof FlashcardSetSideBar>>()
 
-const flashcardSetName = computed(() => {
-  return flashcardSet.value?.name || 'No Flashcard Set Selected'
+const flashcardSetName = computed(() => flashcardSet.value?.name || '')
+const isCurrDayOff = computed(() => currDay.value?.status === chronodayStatuses.OFF)
+const dayNumber = computed(() => isCurrDayOff.value ? '🌴' : currDay.value?.seqNumber ?? '?')
+
+interface StageReview {
+  stage: string
+  count: number
+}
+
+const stageReviews = computed(() => {
+  if (!currDay.value) return []
+
 })
 
 function openFlashcardSetSettings() {
@@ -74,6 +129,8 @@ onMounted(() => {
   --panel--topbar--bg-color: #f5f5f5;
   --panel--topbar--text-color: #333333;
   --panel--topbar--shadow-color: rgba(0, 0, 0, 0.4);
+  --panel--text-color: #333333;
+  --panel--border-color: rgba(0, 0, 0, 0.4);
 }
 
 .control-panel {
@@ -123,10 +180,80 @@ onMounted(() => {
 
 .launch-panel {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 8px;
+  border: 2px solid #0d124a;
+  border-radius: 4px;
+  margin: 4px;
+}
+
+.calendar-panel {
+  display: flex;
+  padding: 4px;
+  gap: 10px;
+}
+
+.calendar-panel__main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 4px;
+}
+
+.calendar-panel__main__button {
   flex: 1;
-  padding: 20px;
+  border: 2px solid #0d124a;
+  border-radius: 6px;
+  --awesome-button--font-size: clamp(40px, 6vw, 50px);
+  --awesome-button--color: #0d124a;
+}
+
+.calendar-panel__main__day {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+}
+
+.calendar-panel__main__day__text {
+  font-size: clamp(0.8rem, 1.8vw, 1rem);
+}
+
+.calendar-panel__main__day__number {
+  font-size: clamp(0.8rem, 1.8vw, 0.9rem);
+  font-weight: 600;
+  border: 1px solid var(--panel--border-color);
+  color: var(--panel--text-color);
+  border-radius: 3px;
+  padding: 2px;
+}
+
+.calendar-panel__main__day__vacation {
+  font-size: clamp(0.8rem, 1.8vw, 1rem);
+  padding: 2px;
+}
+
+.calendar-panel__stages {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4px;
+  border: 2px solid #0d124a;
+  border-radius: 4px;
+  gap: 4px;
+}
+
+.calendar-stage {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid #0d124a;
+  border-radius: 4px;
+  width: 100%;
 }
 </style>
 
