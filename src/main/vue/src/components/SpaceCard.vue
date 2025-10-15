f79d63b070587d40307165a1280c3b5d12f6ad89
elimxim <elimxim@gmail.com>
2025-10-15 15:36:53 +0400

    SpaceCard refactoring

--- src/main/vue/src/components/SpaceCard.vue
+++ src/main/vue/src/components/SpaceCard.vue
@@ -1,33 +1,32 @@
 <template>
   <div
-    class="flashcard flashcard--theme"
-    :class="{ 'flashcard--flipped': flipped }"
+    class="space-card space-card--theme"
+    :class="{ 'space-card--flipped': flipped }"
     @click="flip"
   >
-    <div class="flashcard__flipper">
+    <div class="space-card-flipper">
       <div
-        class="flashcard__face flashcard__face--front"
+        class="space-card-face space-card-face--front"
         :class="{
-          'flashcard__face--front--style': !transparent,
-          'flashcard__face--transparent': transparent,
+          'space-card-face--front--style': !transparent,
+          'space-card-face--transparent': transparent,
         }"
       >
-        <div class="flashcard__strip select-none">
-          <span v-if="!textOnly" class="flashcard__corner-text">
+        <div class="space-card-strip select-none">
+          <span v-if="!textOnly" class="space-card-strip-text">
             {{ stage }}
           </span>
-          <button
+          <AwesomeButton
             v-if="!textOnly"
-            class="flashcard__edit-button"
-            @click.stop="onEdit()"
-          >
-            <font-awesome-icon icon="fa-solid fa-pen-to-square"/>
-          </button>
+            icon="fa-solid fa-pen-to-square"
+            class="space-card-edit-button"
+            :on-click="onEdit"
+          />
         </div>
-        <div class="flashcard__body">
+        <div class="space-card-body">
           {{ frontSide }}
         </div>
-        <div class="flashcard__strip select-none">
+        <div class="space-card-strip select-none">
           <span v-if="!textOnly">
             <font-awesome-icon icon="fa-regular fa-eye"/>
             {{ viewedTimes }}
@@ -35,28 +34,27 @@
         </div>
       </div>
       <div
-        class="flashcard__face flashcard__face--back"
+        class="space-card-face space-card-face--back"
         :class="{
-          'flashcard__face--back--style': !transparent,
-          'flashcard__face--transparent': transparent,
+          'space-card-face--back--style': !transparent,
+          'space-card-face--transparent': transparent,
         }"
       >
-        <div class="flashcard__strip select-none">
-          <span v-if="!textOnly" class="flashcard__corner-text">
+        <div class="space-card-strip select-none">
+          <span v-if="!textOnly" class="space-card-strip-text">
             {{ stage }}
           </span>
-          <button
+          <AwesomeButton
             v-if="!textOnly"
-            class="flashcard__edit-button"
-            @click.stop="onEdit()"
-          >
-            <font-awesome-icon icon="fa-solid fa-pen-to-square"/>
-          </button>
+            icon="fa-solid fa-pen-to-square"
+            class="space-card-edit-button"
+            :on-click="onEdit"
+          />
         </div>
-        <div class="flashcard__body">
+        <div class="space-card-body">
           {{ backSide }}
         </div>
-        <div class="flashcard__strip select-none">
+        <div class="space-card-strip select-none">
           <span v-if="!textOnly">
             <font-awesome-icon icon="fa-regular fa-eye"/>
             {{ viewedTimes }}
@@ -68,6 +66,7 @@
 </template>
 
 <script setup lang="ts">
+import AwesomeButton from '@/components/AwesomeButton.vue'
 import { ref } from 'vue'
 
 const props = withDefaults(defineProps<{
@@ -125,7 +124,20 @@ defineExpose({
 </script>
 
 <style scoped>
-.flashcard {
+.space-card--theme {
+  --card--color: var(--space-card--color, #686868);
+  --card--color--strip: var(--space-card--color--strip, #9f9f9f);
+  --card--color--strip--hover: var(--space-card--color--strip--hover, #686868);
+  --card--border-color: var(--space-card--border-color, none);
+  --card--box-shadow: var(--space-card--box-shadow, 0 8px 12px rgba(0, 0, 0, 0.15));
+  --card--box-shadow--hover: var(--flashcard--box-shadow--hover, 0 12px 16px rgba(0, 0, 0, 0.2));
+  --card--front--bg-color: var(--space-card--front--bg-color, white);
+  --card--front--bg-image: var(--space-card--front--bg-image, none);
+  --card--front--bg-size: var(--space-card--front--bg-size, none);
+  --card--back--bg-color: var(--space-card--back--bg-color, white);
+}
+
+.space-card {
   position: relative;
   width: 100%;
   height: 100%;
@@ -138,21 +150,11 @@ defineExpose({
   z-index: 10;
 }
 
-.flashcard--theme {
-  --default-flashcard__front--bg-color: white;
-  --default-flashcard__back--bg-color: white;
-  --default-flashcard--color: #686868;
-  --default-flashcard--color--strip: #9f9f9f;
-  --default-flashcard--color--strip--hover: #686868;
-  --default-flashcard--box-shadow: 0 8px 12px rgba(0, 0, 0, 0.15);
-  --default-flashcard--box-shadow--hover: 0 12px 16px rgba(0, 0, 0, 0.2);
-}
-
-.flashcard--flipped .flashcard__flipper {
+.space-card--flipped .space-card-flipper {
   transform: rotateY(180deg);
 }
 
-.flashcard__flipper {
+.space-card-flipper {
   flex: 1;
   transition: transform 0.5s cubic-bezier(0.25, 1, 0.5, 1);
   transform-style: preserve-3d;
@@ -160,7 +162,7 @@ defineExpose({
   will-change: transform;
 }
 
-.flashcard__face {
+.space-card-face {
   position: absolute;
   width: 100%;
   height: 100%;
@@ -170,49 +172,49 @@ defineExpose({
   flex-direction: column;
   border-radius: 24px;
   overflow-wrap: break-word;
-  border-color: var(--flashcard--border-color);
+  border-color: var(--card--border-color);
   border-style: solid;
   border-width: 1px;
 }
 
-.flashcard__face--front {
+.space-card-face--front {
   transform: rotateY(0deg);
 }
 
-.flashcard__face--back {
+.space-card-face--back {
   transform: rotateY(180deg);
 }
 
-.flashcard__face--front--style {
-  background-color: var(--flashcard__front--bg-color, var(--default-flashcard__front--bg-color));
-  background-image: var(--flashcard__front--bg-image);
-  background-size: var(--flashcard__front--bg-size);
-  box-shadow: var(--flashcard--box-shadow, var(--default-flashcard--box-shadow));
+.space-card-face--front--style {
+  background-color: var(--card--front--bg-color);
+  background-image: var(--card--front--bg-image);
+  background-size: var(--card--front--bg-size);
+  box-shadow: var(--card--box-shadow);
   transition: box-shadow 0.2s ease-in-out;
 }
 
-.flashcard__face--back--style {
-  background-color: var(--flashcard__back--bg-color, var(--default-flashcard__back--bg-color));
-  box-shadow: var(--flashcard--box-shadow, var(--default-flashcard--box-shadow));
+.space-card-face--back--style {
+  background-color: var(--card--back--bg-color);
+  box-shadow: var(--card--box-shadow);
   transition: box-shadow 0.2s ease-in-out;
 }
 
-.flashcard:hover .flashcard__face--front--style,
-.flashcard:hover .flashcard__face--back--style {
-  box-shadow: var(--flashcard--box-shadow--hover, var(--default-flashcard--box-shadow--hover));
+.space-card:hover .space-card-face--front--style,
+.space-card:hover .space-card-face--back--style {
+  box-shadow: var(--card--box-shadow--hover);
 }
 
-.flashcard__face--transparent {
+.space-card-face--transparent {
   background: none;
   border: none;
   cursor: default;
   perspective: none;
 }
 
-.flashcard__strip {
+.space-card-strip {
   height: fit-content;
   font-size: clamp(1.1rem, 2vw, 1.2rem);
-  color: var(--flashcard--color--strip, var(--default-flashcard--color--strip));
+  color: var(--card--color--strip);
   display: flex;
   justify-content: space-between;
   padding-left: 4px;
@@ -220,12 +222,12 @@ defineExpose({
   gap: 10px;
 }
 
-.flashcard__body {
+.space-card-body {
   flex: 1;
   width: 100%;
   height: 100%;
   font-size: clamp(1.4rem, 2vw, 1.8rem);
-  color: var(--flashcard--color, var(--default-flashcard--color));
+  color: var(--card--color);
   text-align: center;
   white-space: pre-wrap;
   overflow: hidden;
@@ -236,22 +238,16 @@ defineExpose({
   -moz-osx-font-smoothing: grayscale;
 }
 
-.flashcard__corner-text {
+.space-card-strip-text {
   background: none;
   font-size: inherit;
   color: inherit;
 }
 
-.flashcard__edit-button {
-  border: none;
-  outline: none;
-  background: none;
-  cursor: pointer;
-  font-size: inherit;
-  color: inherit;
+.space-card-edit-button {
+  --awesome-button--font-size: inherit;
+  --awesome-button--color: var(--card--color--strip);
+  --awesome-button--color--hover: var(--card--color--strip--hover);
 }
 
-.flashcard__edit-button:hover {
-  color: var(--flashcard--color--strip--hover, var(--default-flashcard--color--strip--hover));
-}
 </style>
