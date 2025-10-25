c9f5f4948f30c72c64e41a778b9601aa6267efe2
elimxim <elimxim@gmail.com>
2025-10-25 17:31:41 +0400

    remove an unused method

--- src/main/kotlin/com/github/elimxim/flashcardsinspace/util/Utils.kt
+++ src/main/kotlin/com/github/elimxim/flashcardsinspace/util/Utils.kt
@@ -1,8 +1,5 @@
 package com.github.elimxim.flashcardsinspace.util
 
-fun List<String>.joinWithBraces() =
-    this.joinToString(separator = "', '", prefix = "'", postfix = "'")
-
 fun String.trimOneLine() = this.trimIndent()
     .split(Regex("\\r?\\n"))
     .filter { it.isNotBlank() }
