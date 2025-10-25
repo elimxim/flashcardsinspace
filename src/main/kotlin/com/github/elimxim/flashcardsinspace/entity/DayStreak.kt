28ed0e201bfee58f236f997561a0dbf04be74c42
elimxim <elimxim@gmail.com>
2025-10-25 17:30:29 +0400

    DayStreak

---
+++ src/main/kotlin/com/github/elimxim/flashcardsinspace/entity/DayStreak.kt
@@ -0,0 +1,22 @@
+package com.github.elimxim.flashcardsinspace.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "day_streak")
+class DayStreak(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    var streak: Int,
+
+    @OneToOne
+    @JoinColumn(name = "chronoday_id", referencedColumnName = "id")
+    var lastDay: Chronoday,
+
+    @OneToOne
+    @JoinColumn(name = "flashcard_set_id", referencedColumnName = "id")
+    var flashcardSet: FlashcardSet,
+)
