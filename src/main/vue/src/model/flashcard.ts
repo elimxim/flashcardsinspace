11175da8a22a14f8c82926a084f9e986858fd1c0
elimxim <elimxim@gmail.com>
2025-10-27 20:57:49 +0400

    frontend flashcard audio model

--- src/main/vue/src/model/flashcard.ts
+++ src/main/vue/src/model/flashcard.ts
@@ -10,13 +10,23 @@ export interface ReviewInfo {
 export interface Flashcard {
   id: number
   frontSide: string
+  frontSideAudioId?: number | undefined
   backSide: string
+  backSideAudioId?: number | undefined
   stage: string
   timesReviewed: number
   reviewHistory: ReviewHistory
   creationDate: string // "yyyy-MM-dd"
   lastReviewDate?: string | undefined // "yyyy-MM-dd"
   lastUpdatedAt?: Date | undefined // ZonedDateTime
+}
+
+export interface FlashcardAudio {
+  id: number
+  side: string
+  mimeType: string
+  audioSize: number
+  uploadedAt?: Date | undefined // ZonedDateTime
 }
 
 export interface FlashcardSet {
