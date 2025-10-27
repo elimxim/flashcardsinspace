300d389ccef00758b8a3e7e8280cb67b7a7ff367
elimxim <elimxim@gmail.com>
2025-10-27 20:57:32 +0400

    case insensitive urls

--- src/main/kotlin/com/github/elimxim/flashcardsinspace/web/WebConfig.kt
+++ src/main/kotlin/com/github/elimxim/flashcardsinspace/web/WebConfig.kt
@@ -6,9 +6,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.util.AntPathMatcher
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig {
+class WebConfig : WebMvcConfigurer {
     @Bean
     fun jacksonCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
         return Jackson2ObjectMapperBuilderCustomizer { builder ->
@@ -20,5 +23,11 @@ class WebConfig {
                 // Enable JavaTimeModule for proper LocalDate and ZonedDateTime serialization
                 .modules(JavaTimeModule())
         }
+    }
+
+    override fun configurePathMatch(configurer: PathMatchConfigurer) {
+        val matcher = AntPathMatcher()
+        matcher.setCaseSensitive(false)
+        configurer.setPathMatcher(matcher)
     }
 }
