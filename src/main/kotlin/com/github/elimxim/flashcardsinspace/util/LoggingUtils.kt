9470f1c9c1d9948ecbaf029efaa4e69f5e45979d
elimxim <elimxim@gmail.com>
2025-10-31 19:32:47 +0400

    Add printApplicationStackTrace

--- src/main/kotlin/com/github/elimxim/flashcardsinspace/util/LoggingUtils.kt
+++ src/main/kotlin/com/github/elimxim/flashcardsinspace/util/LoggingUtils.kt
@@ -2,8 +2,15 @@ package com.github.elimxim.flashcardsinspace.util
 
 import com.github.elimxim.flashcardsinspace.entity.User
 import org.slf4j.MDC
+import java.io.PrintStream
 
+const val APP_PACKAGE = "com.github.elimxim.flashcardsinspace"
 const val USER_ID_MDC_KEY: String = "userId"
+
+inline fun <T> withLoggingContext(block: () -> T): T {
+    val user = getCurrentUser()
+    return withLoggingContext(user?.id, block)
+}
 
 inline fun <T> withLoggingContext(user: User?, block: () -> T): T =
     withLoggingContext(user?.id, block)
@@ -20,6 +27,46 @@ inline fun <T> withLoggingContext(userId: Long?, block: () -> T): T {
     } finally {
         if (previous != null) {
             MDC.put(USER_ID_MDC_KEY, previous)
+        }
+    }
+}
+
+fun Throwable.printApplicationStackTrace() {
+    this.printApplicationStackTrace(System.err)
+}
+
+fun Throwable.printApplicationStackTrace(out: PrintStream) {
+    val user = getCurrentUser()
+
+    val exceptionChain = mutableListOf<Pair<Throwable, List<StackTraceElement>>>()
+    var currentException: Throwable? = this
+    while (currentException != null) {
+        val appStackTraces = currentException.stackTrace.filter {
+            it.className.startsWith(APP_PACKAGE)
+        }
+        if (appStackTraces.isNotEmpty()) {
+            exceptionChain.add(currentException to appStackTraces)
+        }
+        currentException = currentException.cause
+    }
+
+    if (exceptionChain.isEmpty()) {
+        out.println("Application stack trace (${user?.id}): unknown")
+        return
+    }
+
+    out.println("Application stack trace (${user?.id}):")
+    exceptionChain.forEachIndexed { index, (exception, stackTraces) ->
+        val indent = "  ".repeat(index)
+
+        if (index > 0) {
+            out.println("${indent}Caused by: ${exception.javaClass.simpleName}: ${exception.message}")
+        }
+
+        stackTraces.distinctBy {
+            "${it.className}.${it.methodName}:${it.lineNumber}"
+        }.forEach {
+            out.println("$indent  at ${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})")
         }
     }
 }
