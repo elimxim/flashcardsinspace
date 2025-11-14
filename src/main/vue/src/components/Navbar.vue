ce234f8949b5cd851bd21ba410b8b5871c71fc4e
elimxim <elimxim@gmail.com>
2025-11-14 19:49:42 +0400

    adjust colors; add starfield

--- src/main/vue/src/components/Navbar.vue
+++ src/main/vue/src/components/Navbar.vue
@@ -1,15 +1,18 @@
 <template>
-  <nav class="navbar">
+  <nav class="navbar navbar--theme">
+    <Starfield twinkle :density="200" :star-size="1.6" vertical-drift="2px"/>
     <div class="logo-container">
       <img src="@/assets/logo.svg" alt="Logo" class="logo-image"/>
-      <div class="title-text">Flashcards in Space</div>
+      <div class="title-text" @click="navigateToControlPanel">
+        Flashcards in Space
+      </div>
     </div>
 
     <div class="nav-controls">
       <div class="nav-control-item">
         <AwesomeButton
           class="navigation-button"
-          icon="fa-solid fa-shuttle-space"
+          icon="fa-solid fa-rocket"
           :disabled="!isAuthenticated"
           :on-click="navigateToControlPanel"
           tooltip="Control Panel"
@@ -64,6 +67,7 @@
 </template>
 
 <script setup lang="ts">
+import Starfield from '@/components/Starfield.vue'
 import AwesomeButton from '@/components/AwesomeButton.vue'
 import { useAuthStore } from '@/stores/auth-store.ts'
 import { storeToRefs } from 'pinia'
@@ -93,6 +97,10 @@ function navigateToUser() {
 </script>
 
 <style scoped>
+.navbar--theme {
+  --starfield--star--color: #b1b176;
+}
+
 .navbar {
   position: sticky;
   top: 0;
@@ -101,7 +109,7 @@ function navigateToUser() {
   bottom: 0;
   display: flex;
   justify-content: space-between;
-  background-color: #707070;
+  background-color: #0C0404;
   padding: 10px;
   height: var(--navbar-height);
   min-height: var(--navbar-height);
@@ -114,27 +122,39 @@ function navigateToUser() {
   flex-direction: row;
   align-items: center;
   justify-items: center;
-  gap: 10px;
+  gap: 4px;
 }
 
 .nav-control-item {
-  width: 40px;
-  height: 40px;
+  width: 36px;
+  height: 36px;
 }
 
 .logo-container {
   display: flex;
+  flex-direction: row;
   align-items: center;
+  justify-content: center;
+  gap: 10px;
 }
 
 .logo-image {
-  height: 5em;
-  margin-right: 10px;
+  height: 40px;
+  z-index: 1001;
 }
 
 .title-text {
-  font-size: 1.5em;
-  color: white;
-  font-weight: bold;
+  font-size: clamp(1rem, 2vw ,1.5rem);
+  font-weight: 600;
+  letter-spacing: 0.05rem;
+  word-spacing: 0.05rem;
+  text-transform: uppercase;
+  color: #FAF9F6;
+  cursor: pointer;
+  transition: color 0.3s ease-in-out;
+}
+
+.title-text:hover {
+  color: #dad0f8;
 }
 </style>
