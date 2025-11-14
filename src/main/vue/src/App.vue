d20d165c1da0f4084f40a3f7c4f692f6043091cb
elimxim <elimxim@gmail.com>
2025-11-14 18:26:43 +0400

    Navbar

--- src/main/vue/src/App.vue
+++ src/main/vue/src/App.vue
@@ -1,50 +1,17 @@
 <template>
   <div id="app" class="app-container">
-    <nav class="navbar">
-      <div class="logo-container">
-        <img src="@/assets/logo.svg" alt="Logo" class="logo-image"/>
-        <div class="title-text">Flashcards in Space</div>
-      </div>
-
-      <ul class="nav-items">
-        <li :hidden="!isAuthenticated">
-          <router-link :to="{ name: routeNames.controlPanel }">
-            Control Panel
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: routeNames.home }">
-            Home
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: routeNames.support }">
-            Support
-          </router-link>
-        </li>
-        <li class="nav-user">
-          <router-link :to="{ name: routeNames.user }">
-            <font-awesome-icon icon="fa-solid fa-circle-user"/>
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-    <router-view></router-view>
+    <Navbar/>
+    <router-view/>
   </div>
 </template>
 
 <script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
 import { useLanguageStore } from '@/stores/language-store.ts'
-import { useAuthStore } from '@/stores/auth-store.ts'
 import { onMounted } from 'vue'
-import { storeToRefs } from 'pinia'
-import { routeNames } from '@/router'
 import { loadLanguages } from '@/api/public-api-client.ts'
 
-const authStore = useAuthStore()
 const languageStore = useLanguageStore()
-
-const { isAuthenticated } = storeToRefs(authStore)
 
 onMounted(() => {
   loadLanguages().then(response => {
@@ -61,63 +28,6 @@ onMounted(() => {
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-}
-
-.navbar {
-  position: sticky;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: space-between;
-  background-color: #707070;
-  padding: 1vh;
-  height: var(--navbar-height);
-  min-height: var(--navbar-height);
-  max-height: var(--navbar-height);
-}
-
-.nav-items {
-  display: flex;
-  list-style-type: none;
-  align-items: center;
-  margin: 0;
-  padding: 0;
-}
-
-.nav-items li {
-  margin-right: 15px;
-}
-
-.nav-items a {
-  color: white;
-  text-decoration: none;
-}
-
-.nav-items a.router-link-active {
-  font-weight: bold;
-  text-decoration: underline;
-}
-
-.nav-user {
-  font-size: 1.6em;
-}
-
-.logo-container {
-  display: flex;
-  align-items: center;
-}
-
-.logo-image {
-  height: 5em;
-  margin-right: 10px;
-}
-
-.title-text {
-  font-size: 1.5em;
-  color: white;
-  font-weight: bold;
 }
 
 </style>
---
+++ src/main/vue/src/components/Navbar.vue
@@ -0,0 +1,136 @@
+<template>
+  <nav class="navbar">
+    <div class="logo-container">
+      <img src="@/assets/logo.svg" alt="Logo" class="logo-image"/>
+      <div class="title-text">Flashcards in Space</div>
+    </div>
+
+    <div class="nav-controls">
+      <div class="nav-control-item">
+        <AwesomeButton
+          class="navigation-button"
+          icon="fa-solid fa-shuttle-space"
+          :disabled="!isAuthenticated"
+          :on-click="navigateToControlPanel"
+          tooltip="Control Panel"
+          :tooltip-delay="10"
+          tooltip-position="bottom"
+          fill-space
+          square
+        />
+      </div>
+      <div class="nav-control-item">
+        <AwesomeButton
+          class="navigation-button"
+          icon="fa-solid fa-house"
+          :on-click="navigateToHome"
+          tooltip="Home"
+          :tooltip-delay="10"
+          tooltip-position="bottom"
+          fill-space
+          square
+        />
+      </div>
+      <div class="nav-control-item">
+        <AwesomeButton
+          class="navigation-button"
+          icon="fa-solid fa-heart-pulse"
+          :on-click="navigateToSupport"
+          tooltip="Support"
+          :tooltip-delay="10"
+          tooltip-position="bottom"
+          fill-space
+          square
+        />
+      </div>
+      <div class="nav-control-item">
+        <AwesomeButton
+          class="navigation-button"
+          icon="fa-solid fa-user-astronaut"
+          :on-click="navigateToUser"
+          tooltip="User"
+          :tooltip-delay="10"
+          tooltip-position="bottom-left"
+          fill-space
+          square
+        />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import AwesomeButton from '@/components/AwesomeButton.vue'
+import { useAuthStore } from '@/stores/auth-store.ts'
+import { storeToRefs } from 'pinia'
+import { routeNames } from '@/router'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const { isAuthenticated } = storeToRefs(authStore)
+
+function navigateToControlPanel() {
+  router.push({ name: routeNames.controlPanel })
+}
+
+function navigateToHome() {
+  router.push({ name: routeNames.home })
+}
+
+function navigateToSupport() {
+  router.push({ name: routeNames.support })
+}
+
+function navigateToUser() {
+  router.push({ name: routeNames.user })
+}
+</script>
+
+<style scoped>
+.navbar {
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  background-color: #707070;
+  padding: 10px;
+  height: var(--navbar-height);
+  min-height: var(--navbar-height);
+  max-height: var(--navbar-height);
+  z-index: 1000;
+}
+
+.nav-controls {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-items: center;
+  gap: 10px;
+}
+
+.nav-control-item {
+  width: 36px;
+  height: 36px;
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+}
+
+.logo-image {
+  height: 5em;
+  margin-right: 10px;
+}
+
+.title-text {
+  font-size: 1.5em;
+  color: white;
+  font-weight: bold;
+}
+</style>
