3775ac898f3f30a3c857db5b2dfe5549f837f609
elimxim <elimxim@gmail.com>
2025-11-14 23:48:20 +0400

    userSignedUp cookie

--- src/main/vue/src/shared/cookies.ts
+++ src/main/vue/src/shared/cookies.ts
@@ -1,18 +1,40 @@
 import Cookies from 'js-cookie'
 
-export function saveSelectedSetId(value: number) {
-  Cookies.set('selectedSetId', value.toString(), {
+const COOKIE_SELECTED_SET_ID = 'selectedSetId'
+const COOKIE_USER_SIGNED_UP = 'userSignedUp'
+
+export function saveUserSignedUp(value: boolean) {
+  Cookies.set(COOKIE_USER_SIGNED_UP, value.toString(), {
     expires: 30, // 30 days
     sameSite: 'Lax',
     secure: true,
     path: '/', // valid for the entire site
   })
-  console.log( `${value} => Cookie.selectedSetId`)
+  console.log( `${value} => Cookie.${COOKIE_USER_SIGNED_UP}`)
+}
+
+export function loadUserSignedUp(): boolean {
+  const value = Cookies.get(COOKIE_USER_SIGNED_UP)
+  console.log(`Cookie.${COOKIE_USER_SIGNED_UP} => ${value}`)
+  if (value) {
+    return value === 'true'
+  }
+  return false
+}
+
+export function saveSelectedSetId(value: number) {
+  Cookies.set(COOKIE_SELECTED_SET_ID, value.toString(), {
+    expires: 30, // 30 days
+    sameSite: 'Lax',
+    secure: true,
+    path: '/', // valid for the entire site
+  })
+  console.log( `${value} => Cookie.${COOKIE_SELECTED_SET_ID}`)
 }
 
 export function loadSelectedSetId(): number | undefined {
-  const value = Cookies.get('selectedSetId')
-  console.log(`Cookie.selectedSetId => ${value}`)
+  const value = Cookies.get(COOKIE_SELECTED_SET_ID)
+  console.log(`Cookie.${COOKIE_SELECTED_SET_ID} => ${value}`)
   if (value) {
     return parseInt(value)
   }
