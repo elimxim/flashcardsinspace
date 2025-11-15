102107109a2043391b07694bbc6822fb439ee0ef
elimxim <elimxim@gmail.com>
2025-11-15 18:18:19 +0400

    attempt token refresh on load or reload a protected page

--- src/main/vue/src/main.ts
+++ src/main/vue/src/main.ts
@@ -8,6 +8,7 @@ import { useAuthStore } from '@/stores/auth-store.ts'
 import { sendWhoAmIRequest } from '@/api/auth-client.ts'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { applyFaIcons } from '@/fa.ts'
+import { attemptTokenRefresh } from '@/api/token-refresh.ts'
 
 applyFaIcons()
 
@@ -17,15 +18,17 @@ app.use(createPinia())
 
 const authStore = useAuthStore()
 
-sendWhoAmIRequest().then(response => {
-  if (response.status === 200) {
-    console.log('Who Am I: ', response.data)
-    authStore.setUser(response.data)
-  } else {
-    console.log('Who Am I: ', response.status)
-    authStore.resetUser()
-  }
-}).then(() => {
-  app.use(router)
-  app.mount('#app')
-})
+sendWhoAmIRequest()
+  .then(async (response) => {
+    if (response.status === 200) {
+      console.log('Who Am I: ', response.data)
+      authStore.setUser(response.data)
+    } else {
+      console.log('Who Am I: ', response.status)
+      await attemptTokenRefresh()
+    }
+  })
+  .then(() => {
+    app.use(router)
+    app.mount('#app')
+  })
