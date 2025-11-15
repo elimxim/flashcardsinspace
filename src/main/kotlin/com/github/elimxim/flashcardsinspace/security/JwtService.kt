32bd817764525fd36ad1e513d1a416a84e0ce8bd
elimxim <elimxim@gmail.com>
2025-11-15 17:07:30 +0400

    Actualize refresh token URL path

--- src/main/kotlin/com/github/elimxim/flashcardsinspace/security/JwtService.kt
+++ src/main/kotlin/com/github/elimxim/flashcardsinspace/security/JwtService.kt
@@ -41,7 +41,7 @@ class JwtService(private val securityProperties: SecurityProperties) {
         val refreshTokenCookie = Cookie("refreshToken", refreshToken).apply {
             isHttpOnly = true
             secure = true
-            path = "/auth/refresh-token"
+            path = "/auth/refresh"
             maxAge = (securityProperties.jwt.refreshTokenExpirationMs / 1000).toInt()
         }
 
@@ -60,7 +60,7 @@ class JwtService(private val securityProperties: SecurityProperties) {
         val refreshTokenCookie = Cookie("refreshToken", null).apply {
             isHttpOnly = true
             secure = true
-            path = "/auth/refresh-token"
+            path = "/auth/refresh"
             maxAge = 0
         }
 
