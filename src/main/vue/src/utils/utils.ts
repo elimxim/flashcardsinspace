849856a31aff2f4c460411d50c67df20334c237a
elimxim <elimxim@gmail.com>
2025-11-17 02:09:19 +0400

    documented function mulberry32

--- src/main/vue/src/utils/utils.ts
+++ src/main/vue/src/utils/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Creates a seeded pseudorandom number generator using the Mulberry32 algorithm.
+ *
+ * @param a - The seed value (32-bit integer)
+ * @returns A function that generates the next pseudorandom number in the sequence.
+ *          Each call returns a floating-point number in the range [0, 1).
+ */
 export function mulberry32(a: number) {
   return function () {
     let t = (a += 0x6D2B79F5)
