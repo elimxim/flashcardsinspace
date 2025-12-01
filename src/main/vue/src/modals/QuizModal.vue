4663f74664397c84507d302636321d62a6a11d73
elimxim <elimxim@gmail.com>
2025-12-01 17:11:29 +0400

    QuizModal (empty)

---
+++ src/main/vue/src/modals/QuizModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <Modal
+    :visible="toggleStore.quizOpen"
+    :on-press-exit="exit"
+  >
+    <div>
+      Quiz
+    </div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from '@/components/Modal.vue'
+import { useToggleStore } from '@/stores/toggle-store.ts'
+
+const toggleStore = useToggleStore()
+
+function exit() {
+  toggleStore.toggleQuiz()
+}
+</script>
+
+<style scoped>
+
+</style>
