e97b75d3c4b54681996d7a4f514a111956ffddf0
elimxim <elimxim@gmail.com>
2025-12-07 21:53:45 +0400

    add a review session service with implemented review session creation and update logic

---
+++ src/main/kotlin/com/github/elimxim/flashcardsinspace/service/ReviewSessionService.kt
@@ -0,0 +1,187 @@
+package com.github.elimxim.flashcardsinspace.service
+
+import com.github.elimxim.flashcardsinspace.entity.QuizMetadata
+import com.github.elimxim.flashcardsinspace.entity.ReviewSession
+import com.github.elimxim.flashcardsinspace.entity.ReviewSessionType
+import com.github.elimxim.flashcardsinspace.entity.User
+import com.github.elimxim.flashcardsinspace.entity.repository.ReviewSessionRepository
+import com.github.elimxim.flashcardsinspace.service.validation.RequestValidator
+import com.github.elimxim.flashcardsinspace.util.getMetadataFieldName
+import com.github.elimxim.flashcardsinspace.util.numbersOnlyPattern
+import com.github.elimxim.flashcardsinspace.web.dto.*
+import com.github.elimxim.flashcardsinspace.web.exception.ParentReviewSessionIsNotQuizException
+import com.github.elimxim.flashcardsinspace.web.exception.ReviewSessionNotFoundException
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.ZonedDateTime
+
+private val log = LoggerFactory.getLogger(ReviewSessionService::class.java)
+
+@Service
+class ReviewSessionService(
+    private val requestValidator: RequestValidator,
+    private val flashcardSetService: FlashcardSetService,
+    private val chronoService: ChronoService,
+    private val reviewSessionRepository: ReviewSessionRepository,
+) {
+
+    @Transactional
+    fun create(user: User, setId: Long, request: ReviewSessionCreateRequest): ReviewSessionResponse {
+        log.info("Creating a new review session for set $setId")
+        flashcardSetService.verifyUserHasAccess(user, setId)
+        flashcardSetService.verifyInitialized(setId)
+        return create(setId, requestValidator.validate(request))
+    }
+
+    @Transactional
+    fun create(setId: Long, request: ValidReviewSessionCreateRequest): ReviewSessionResponse {
+        val session = createReviewSession(setId, request)
+        val savedSession = reviewSessionRepository.save(session)
+        if (savedSession.type == ReviewSessionType.QUIZ) {
+            addQuizMetadata(savedSession)
+        }
+        return ReviewSessionResponse(
+            reviewSessionId = savedSession.id,
+        )
+    }
+
+    @Transactional
+    fun update(user: User, setId: Long, id: Long, request: ReviewSessionUpdateRequest) {
+        log.info("Updating review session $id for set $setId")
+        flashcardSetService.verifyUserHasAccess(user, setId)
+        flashcardSetService.verifyInitialized(setId)
+        update(id, requestValidator.validate(request))
+    }
+
+    @Transactional
+    fun update(id: Long, request: ValidReviewSessionUpdateRequest) {
+        val session = getEntity(id)
+        val changed = mergeReviewSession(session, request)
+        if (changed) {
+            reviewSessionRepository.save(session)
+        }
+    }
+
+    private fun mergeReviewSession(session: ReviewSession, request: ValidReviewSessionUpdateRequest): Boolean {
+        var changed = false
+        if (request.elapsedTime > session.elapsedTime) {
+            session.elapsedTime = request.elapsedTime
+            changed = true
+        }
+
+        if (request.flashcardIds.isNotEmpty()) {
+            val flashcardIds = (session.flashcardIds?.toSet() ?: emptySet()) + request.flashcardIds
+            session.flashcardIds = flashcardIds.toLongArray()
+            changed = true
+        }
+
+        if (session.type == ReviewSessionType.QUIZ) {
+            changed = updateQuizMetadata(session, request.metadata)
+        }
+
+        return changed
+    }
+
+    @Transactional
+    fun createChild(
+        user: User, setId: Long, parentId: Long,
+        request: ReviewSessionCreateRequest
+    ): ReviewSessionResponse {
+        log.info("Creating a child review session of $parentId for set $setId")
+        flashcardSetService.verifyUserHasAccess(user, setId)
+        flashcardSetService.verifyInitialized(setId)
+        return createChild(setId, parentId, requestValidator.validate(request))
+    }
+
+    @Transactional
+    fun createChild(setId: Long, parentId: Long, request: ValidReviewSessionCreateRequest): ReviewSessionResponse {
+        val parentSession = getEntity(parentId)
+        val session = createReviewSession(setId, request).apply {
+            parentSessionId = parentSession.id
+        }
+        if (session.type == ReviewSessionType.QUIZ) {
+            if (parentSession.type != ReviewSessionType.QUIZ) {
+                throw ParentReviewSessionIsNotQuizException(
+                    "Parent session ${parentSession.id} type is not ${ReviewSessionType.QUIZ}"
+                )
+            }
+            addQuizMetadata(session)
+        }
+        val savedSession = reviewSessionRepository.save(session)
+        return ReviewSessionResponse(
+            reviewSessionId = savedSession.id,
+        )
+    }
+
+    @Transactional
+    fun getEntity(id: Long): ReviewSession {
+        return reviewSessionRepository.findById(id).orElseThrow {
+            ReviewSessionNotFoundException("Review session $id not found")
+        }
+    }
+
+    private fun createReviewSession(setId: Long, request: ValidReviewSessionCreateRequest): ReviewSession {
+        return ReviewSession(
+            type = request.type,
+            flashcardIds = request.flashcardIds.toLongArray(),
+            elapsedTime = 0,
+            startedAt = ZonedDateTime.now(),
+            flashcardSet = flashcardSetService.getEntity(setId),
+            reviewDay = chronoService.getEntity(request.chronodayId),
+        )
+    }
+
+    private fun addQuizMetadata(session: ReviewSession, parentSession: ReviewSession? = null) {
+        val metadata = if (parentSession != null && parentSession.metadata != null) {
+            val parentMetadata = parentSession.metadata
+            if (parentMetadata !is QuizMetadata) {
+                throw ParentReviewSessionIsNotQuizException(
+                    "Parent session ${parentSession.id} metadata is not ${ReviewSessionType.QUIZ}"
+                )
+            }
+
+            QuizMetadata(
+                round = parentMetadata.round + 1,
+                overallCorrectCount = parentMetadata.overallCorrectCount,
+                overallTotalCount = parentMetadata.overallTotalCount,
+            )
+        } else {
+            QuizMetadata(
+                round = 1,
+                overallCorrectCount = 0,
+                overallTotalCount = session.flashcardIds!!.size,
+            )
+        }
+
+        session.metadata = metadata
+    }
+
+    private fun updateQuizMetadata(session: ReviewSession, metadata: Map<String, String>): Boolean {
+        val sessionMetadata = session.metadata
+        if (sessionMetadata == null) {
+            log.warn("Review session ${session.id} has no metadata")
+            return false
+        }
+
+        if (sessionMetadata !is QuizMetadata) {
+            log.warn("Review session ${session.id} metadata is not ${ReviewSessionType.QUIZ}")
+            return false
+        }
+
+        val nextRoundFlashcardIdsFieldName = getMetadataFieldName(QuizMetadata::nextRoundFlashcardIds)
+        if (nextRoundFlashcardIdsFieldName != null) {
+            val nextRoundFlashcardIds = metadata[nextRoundFlashcardIdsFieldName]
+            if (nextRoundFlashcardIds != null) {
+                val ids = nextRoundFlashcardIds.split(",")
+                    .filter { it.matches(numbersOnlyPattern) }
+                    .map { it.toLong() }
+
+                sessionMetadata.nextRoundFlashcardIds.addAll(ids)
+                return ids.isNotEmpty()
+            }
+        }
+
+        return false
+    }
+}
