653c0db2618f9c57aa851e767d59dbd0e3acc1c5
elimxim <elimxim@gmail.com>
2025-12-08 20:32:06 +0400

    fix tests

--- src/test/kotlin/com/github/elimxim/flashcardsinspace/TestConfigurationContext.kt
+++ src/test/kotlin/com/github/elimxim/flashcardsinspace/TestConfigurationContext.kt
@@ -45,4 +45,7 @@ class TestConfigurationContext {
 
     @Bean
     fun dayStreakRepository() = mockk<DayStreakRepository>()
+
+    @Bean
+    fun reviewSessionRepository() = mockk<ReviewSessionRepository>()
 }
