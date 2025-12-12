bc1f80ed20388705c656110fa57ed69173178c60
elimxim <elimxim@gmail.com>
2025-12-12 04:18:58 +0400

    add quizSessionIdCookie ref

--- src/main/vue/src/utils/cookies-ref.ts
+++ src/main/vue/src/utils/cookies-ref.ts
@@ -1,15 +1,28 @@
 import { ref, watch } from 'vue'
 import {
   loadSidebarExpandedFromCookies,
-  setSidebarExpandedToCookies
+  setSidebarExpandedToCookies,
+  loadQuizSessionIdFromCookies,
+  saveQuizSessionIdToCookies,
+  removeQuizSessionIdFromCookies,
 } from '@/utils/cookies.ts'
 
 const sidebarExpandedCookie = ref(loadSidebarExpandedFromCookies())
+const quizSessionIdCookie = ref(loadQuizSessionIdFromCookies())
 
 watch(sidebarExpandedCookie, (newVal) => {
   setSidebarExpandedToCookies(newVal)
 })
 
+watch(quizSessionIdCookie, (newVal) => {
+  if (newVal) {
+    saveQuizSessionIdToCookies(newVal)
+  } else {
+    removeQuizSessionIdFromCookies()
+  }
+})
+
 export {
   sidebarExpandedCookie,
+  quizSessionIdCookie,
 }
