567943749c5ace8e959fc89f08f8079d510bbc34
elimxim <elimxim@gmail.com>
2026-01-02 18:30:11 +0400

    remove duplicated theme class

--- src/main/vue/src/components/ControlBar.vue
+++ src/main/vue/src/components/ControlBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="control-bar control-bar--theme"
+    class="control-bar"
     :class="{ 'shadow': shadow }"
   >
     <div class="left-controls">
@@ -37,13 +37,6 @@ const centerTitlePaddingPx = computed(() => `${props.centerTitlePadding}px`)
 </script>
 
 <style scoped>
-.control-bar--theme {
-  --c-bar--bg-color: var(--control-bar--bg-color, #f5f5f5);
-  --c-bar--title--color: var(--control-bar--title--color, #333333);
-  --c-bar--title--font-size: var(--control-bar--title--font-size, clamp(1rem, 1.8vw, 1.2rem));
-  --c-bar--shadow-color: var(--control-bar--shadow-color, rgba(0, 0, 0, 0.15));
-}
-
 .control-bar {
   position: relative;
   display: grid;
@@ -51,13 +44,13 @@ const centerTitlePaddingPx = computed(() => `${props.centerTitlePadding}px`)
   align-items: center;
   gap: 12px;
   padding: 4px 10px;
-  background-color: var(--c-bar--bg-color);
+  background-color: var(--control-bar--bg-color);
   width: 100%;
   height: 40px;
 }
 
 .control-bar.shadow {
-  box-shadow: 0 3px 3px 0 var(--c-bar--shadow-color);
+  box-shadow: 0 3px 3px 0 var(--control-bar--shadow-color);
 }
 
 .left-controls {
@@ -86,8 +79,8 @@ const centerTitlePaddingPx = computed(() => `${props.centerTitlePadding}px`)
   grid-column: 2;
   grid-row: 1;
   text-align: left;
-  color: var(--c-bar--title--color);
-  font-size: var(--c-bar--title--font-size);
+  color: var(--control-bar--title--color);
+  font-size: var(--control-bar--title--font-size);
   font-weight: 300;
   overflow: hidden;
   text-overflow: ellipsis;
