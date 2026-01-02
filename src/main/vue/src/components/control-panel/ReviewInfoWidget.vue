b5ab44dbd7ecc37c51dd501b7a2e3e344d137f2b
elimxim <elimxim@gmail.com>
2026-01-02 17:24:42 +0400

    apply control panel theme to ReviewInfoWidget

--- src/main/vue/src/components/control-panel/ReviewInfoWidget.vue
+++ src/main/vue/src/components/control-panel/ReviewInfoWidget.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="review-info-widget review-info-widget--theme">
-    <div class="review-info-title select-none">
+  <div class="review-info-widget">
+    <div class="cp--text cp--text--nowrap">
       To review
     </div>
-    <div class="review-list select-none">
+    <div class="review-list">
       <div
         v-for="review in currDayStageReviews"
         :key="review.stage"
         class="review-item"
       >
-        <div class="review-item-label">
+        <div class="cp--text cp--text--left cp--text--nowrap">
           {{ review.stage }}
         </div>
-        <div class="review-item-count">
+        <div class="cp--count-box">
           {{ review.count }}
         </div>
       </div>
       <div class="review-item review-item--total">
-        <div class="review-item-label">
+        <div class="cp--text cp--text--left cp--text--nowrap">
           Total
         </div>
-        <div class="review-item-count">
+        <div class="cp--count-box">
           {{ currDayReviewTotal }}
         </div>
       </div>
@@ -34,6 +34,7 @@ import { useChronoStore } from '@/stores/chrono-store.ts'
 import { storeToRefs } from 'pinia'
 import { computed } from 'vue'
 import { calcStageReviews, StageReview } from '@/core-logic/review-logic.ts'
+
 const flashcardStore = useFlashcardStore()
 const chronoStore = useChronoStore()
 
@@ -51,38 +52,17 @@ const currDayReviewTotal = computed(() =>
 </script>
 
 <style scoped>
-.review-info-widget--theme {
-  --r-widget--bg: var(--review-info-widget--bg, linear-gradient(135deg, rgb(154, 170, 241) 0%, rgb(162, 133, 192) 100%));
-  --r-widget--border-color: var(--review-info-widget--border-color, rgba(128, 128, 128, 0.62));
-  --r-widget--title--color: var(--review-info-widget--title--color, rgba(13, 18, 74, 0.6));
-  --r-widget--label--color: var(--review-info-widget--label--color, rgba(13, 18, 74, 0.6));
-  --r-widget--number--color: var(--review-info-widget--number--color, rgba(20, 27, 106, 0.82));
-  --r-widget--number--bg: var(--review-info-widget--number--bg, rgba(255, 255, 255, 0.6));
-}
-
 .review-info-widget {
   position: relative;
   display: flex;
   flex-direction: column;
   height: 100%;
   overflow: hidden;
-  background: var(--r-widget--bg);
-  border: 1px solid var(--r-widget--border-color);
+  background: var(--cp--widget--color);
+  border: 1px solid var(--cp--border-color);
   border-radius: 6px;
   padding: 4px;
-}
-
-.review-info-title {
-  font-size: 0.9rem;
-  font-weight: 600;
-  color: var(--r-widget--title--color);
-  letter-spacing: 0.05rem;
-  word-spacing: 0.05rem;
-  text-transform: uppercase;
-  text-align: center;
-  white-space: nowrap;
-  padding: 2px;
-  flex-shrink: 0;
+  gap: 4px;
 }
 
 .review-list {
@@ -106,25 +86,4 @@ const currDayReviewTotal = computed(() =>
   font-weight: 600;
 }
 
-.review-item-label {
-  font-size: 0.9rem;
-  word-spacing: 0.05rem;
-  letter-spacing: 0.05rem;
-  text-transform: uppercase;
-  white-space: nowrap;
-  color: var(--r-widget--label--color);
-  flex: 1;
-  text-align: left;
-}
-
-.review-item-count {
-  font-size: 0.85rem;
-  font-weight: 600;
-  color: var(--r-widget--number--color);
-  background: var(--r-widget--number--bg);
-  border-radius: 4px;
-  padding: 2px;
-  width: 40px;
-  text-align: center;
-}
 </style>
