b76a55ecbafac8c1e8c3d644f59497bb1b2adda3
elimxim <elimxim@gmail.com>
2026-01-02 15:10:37 +0400

    apply control panel theme to DayStreakWidget

--- src/main/vue/src/components/control-panel/DayStreakWidget.vue
+++ src/main/vue/src/components/control-panel/DayStreakWidget.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="day-streak-widget day-streak-widget--theme">
+  <div class="day-streak-widget">
     <div class="day-streak select-none">
       <div class="day-steak-number">
         {{ dayStreak }}
@@ -21,14 +21,6 @@ const { dayStreak } = storeToRefs(chronoStore)
 </script>
 
 <style scoped>
-.day-streak-widget--theme {
-  --d-widget--font-family: var(--day-streak--font-family);
-  --d-widget--number--color: var(--day-streak-widget--number--color, rgba(255, 255, 255, 0.6));
-  --d-widget--text--color: var(--day-streak-widget--text--color, rgba(255, 255, 255, 0.6));
-  --d-widget--bg: var(--day-streak-widget--bg, linear-gradient(135deg, rgb(142, 110, 189) 0%, rgb(215, 91, 209) 100%));
-  --d-widget--border-color: var(--day-streak-widget--border-color, rgba(128, 128, 128, 0.62));
-}
-
 .day-streak-widget {
   position: relative;
   display: flex;
@@ -43,15 +35,15 @@ const { dayStreak } = storeToRefs(chronoStore)
   gap: 8px;
   padding: 4px;
   border-radius: 6px;
-  border: 1px solid var(--d-widget--border-color);
-  background: var(--d-widget--bg);
+  border: 1px solid var(--main-panel--widget--border-color);
+  background: var(--main-panel--widget--color--active);
   height: 100%;
 }
 
 .day-steak-number {
   margin-top: 5px;
-  color: var(--d-widget--number--color);
-  font-family: var(--d-widget--font-family);
+  color: var(--main-panel--widget--text--color--active);
+  font-family: var(--day-streak--font-family);
   font-weight: 600;
   font-variant-numeric: tabular-nums lining-nums;
   font-size: 3rem;
@@ -61,7 +53,7 @@ const { dayStreak } = storeToRefs(chronoStore)
 }
 
 .day-streak-text {
-  color: var(--d-widget--text--color);
+  color: var(--main-panel--widget--text--color--active);
   font-weight: 600;
   font-size: 0.9rem;
   word-spacing: 0.05rem;
