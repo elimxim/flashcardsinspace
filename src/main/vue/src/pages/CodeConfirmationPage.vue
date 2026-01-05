f462cda959f5b9a5b0a32ab5dc32884fb3ad24ef
elimxim <elimxim@gmail.com>
2026-01-05 16:26:41 +0400

    CodeConfirmationPage component

---
+++ src/main/vue/src/pages/CodeConfirmationPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    :class="[
+      'page',
+      'flex-row',
+      'padding-auto',
+      'flex-center',
+      'home-page',
+      'scroll-none',
+      'touch-none'
+    ]"
+  >
+    <div class="hud-wrapper">
+      <CodeConfirmationDevice/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CodeConfirmationDevice from '@/components/CodeConfirmationDevice.vue'
+
+
+</script>
+
+<style scoped>
+.hud-wrapper {
+  height: 80vh;
+  width: 90vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
