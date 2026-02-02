8584fd249125ccd7dfa7fe9af21b32d4bdbc5a6a
elimxim <elimxim@gmail.com>
2026-02-02 23:53:33 +0400

    UserPage: UserInfo (async loading) plus UserInfoSkeleton

---
+++ src/main/vue/src/pages/UserInfoSkeleton.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="user-info-skeleton">
+    <div class="skeleton user-info-row-skeleton"/>
+    <div class="skeleton user-info-row-skeleton"/>
+    <div class="skeleton user-info-row-skeleton"/>
+    <div class="skeleton user-info-row-skeleton--thin"/>
+    <div class="skeleton user-info-row-skeleton--thin user-info-row-skeleton--short"/>
+  </div>
+  <div class="skeleton user-controls-skeleton"/>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.user-info-skeleton {
+  flex: 0 0 auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, auto);
+  grid-auto-flow: column;
+  gap: 10px;
+  max-width: 600px;
+}
+
+.user-info-row-skeleton {
+  height: 42px;
+  width: 100%;
+  border-radius: 6px;
+}
+
+.user-info-row-skeleton--thin {
+  height: 16px;
+}
+
+.user-info-row-skeleton--short {
+  width: 50%;
+}
+
+.user-controls-skeleton {
+  margin-top: 20px;
+  width: 100%;
+  height: 40px;
+  border-radius: 6px;
+  max-width: 600px;
+}
+
+</style>
--- src/main/vue/src/pages/UserPage.vue
+++ src/main/vue/src/pages/UserPage.vue
@@ -12,127 +12,30 @@
   >
     <h2>Welcome aboard, {{ user?.name ?? 'Unknown' }}!</h2>
     <div>{{ randomDayPhrase }}</div>
-    <div class="user-info">
-      <div class="user-info-item">
-        <AwesomeContainer icon="fa-solid fa-user-astronaut" class="user-info-icon">
-          <SmartInput
-            v-model="username"
-            type="text"
-            name="name"
-            placeholder="Name"
-            :invalid="usernameInvalid"
-          />
-        </AwesomeContainer>
-        <ErrorText
-          :show="usernameRegexMismatch"
-          :errors="[
-              {
-                when: usernameRegexMismatch,
-                text: 'Please use only letters, numbers, dashes, underscores, and spaces'
-              },
-              {
-                when: usernameMaxLengthInvalid,
-                text: 'This username is expanding faster than the universe! Please keep it under 64 characters'
-              },
-            ]"
-        />
-      </div>
-      <div class="user-info-item">
-        <AwesomeContainer icon="fa-solid fa-envelope" class="user-info-icon">
-          <SmartInput
-            v-model="userEmail"
-            type="email"
-            name="username"
-            placeholder="Email"
-            :invalid="userEmailInvalid"
-            readonly
-          />
-        </AwesomeContainer>
-        <ErrorText
-          :when="userEmailWrongFormat"
-          text="This email seems to be lost in a cosmic dust cloud. Please check the format"
-        />
-      </div>
-      <div class="user-info-item">
-        <AwesomeContainer icon="fa-solid fa-globe" class="user-info-icon">
-          <FuzzySelect
-            id="language"
-            v-model="language"
-            :options="languages"
-            :option-label="(lang) => lang.name"
-            :invalid="languageInvalid"
-            option-placeholder="Language"
-            search-placeholder="Search..."
-          />
-        </AwesomeContainer>
-      </div>
-      <div class="user-info-item">
-        Registered at: {{ user?.registeredAt ?? 'Unknown' }}
-      </div>
-      <div class="user-info-item">
-        Timezone: {{ user?.timezone ?? 'Unknown' }}
-      </div>
-      <div class="user-controls">
-        <SmartButton
-          text="Logout"
-          class="off-button"
-          :on-click="logout"
-          fill-width
-          auto-blur
-        />
-        <SmartButton
-          text="Save"
-          class="calm-button"
-          :on-click="save"
-          :disabled="!stateChanged || validationFailed"
-          auto-blur
-        />
-      </div>
-    </div>
+    <Suspense>
+      <template #default>
+        <UserInfo :user="user"/>
+      </template>
+      <template #fallback>
+        <UserInfoSkeleton/>
+      </template>
+    </Suspense>
   </div>
   <SpaceToast/>
 </template>
 
 
 <script setup lang="ts">
-import AwesomeContainer from '@/components/AwesomeContainer.vue'
-import SmartInput from '@/components/SmartInput.vue'
-import SmartButton from '@/components/SmartButton.vue'
-import FuzzySelect from '@/components/FuzzySelect.vue'
+import UserInfo from '@/pages/UserInfo.vue'
+import UserInfoSkeleton from '@/pages/UserInfoSkeleton.vue'
 import SpaceToast from '@/components/SpaceToast.vue'
-import ErrorText from '@/components/ErrorText.vue'
 import { useAuthStore } from '@/stores/auth-store.ts'
-import { useLanguageStore } from '@/stores/language-store.ts'
 import { storeToRefs } from 'pinia'
-import { computed, onMounted, ref, watch } from 'vue'
-import { Language } from '@/model/language.ts'
-import { useVuelidate } from '@vuelidate/core'
-import { email, helpers, maxLength, required } from '@vuelidate/validators'
-import { loadLanguageStore } from '@/utils/stores.ts'
-import { sendUserUpdateRequest } from '@/api/api-client.ts'
-import { useSpaceToaster } from '@/stores/toast-store.ts'
-import { routeNames } from '@/router'
-import { useRouter } from 'vue-router'
-import { Log, LogTag } from '@/utils/logger.ts'
-import { userApiErrors } from '@/api/user-api-error.ts'
+import { computed } from 'vue'
 
 const authStore = useAuthStore()
-const languageStore = useLanguageStore()
-const toaster = useSpaceToaster()
-const router = useRouter()
 
 const { user } = storeToRefs(authStore)
-const { languages } = storeToRefs(languageStore)
-
-const username = ref(user.value?.name ?? 'Unknown')
-const userEmail = ref(user.value?.email ?? 'Unknown')
-const language = ref<Language | undefined>()
-
-const stateChanged = computed(() => {
-  return username.value !== user.value?.name ||
-    userEmail.value !== user.value?.email ||
-    language.value?.id !== user.value?.languageId
-})
 
 const daysSinceRegistration = computed(() => {
   if (!user.value?.registeredAt) {
@@ -157,84 +60,6 @@ const randomDayPhrase = computed(() => {
   return daysPhrases[randomIndex](daysSinceRegistration.value)
 })
 
-const $v = useVuelidate({
-  username: {
-    required,
-    maxLength: maxLength(64),
-    regex: helpers.regex(/^[A-Za-z0-9 _-]+$/),
-  },
-  userEmail: { required, email },
-  language: { required },
-}, {
-  username: username,
-  userEmail: userEmail,
-  language: language,
-})
-
-const validationFailed = computed(() => $v.value.$errors.length > 0)
-const usernameInvalid = computed(() => $v.value.username.$errors.length > 0)
-const usernameRegexMismatch = computed(() =>
-  $v.value.username.$errors.find(v => v.$validator === 'regex') !== undefined
-)
-const usernameMaxLengthInvalid = computed(() =>
-  $v.value.username.$errors.find(v => v.$validator === 'maxLength') !== undefined
-)
-const userEmailInvalid = computed(() => $v.value.userEmail.$errors.length > 0)
-const userEmailWrongFormat = computed(() =>
-  $v.value.userEmail.$errors.find(v => v.$validator === 'email') !== undefined
-)
-const languageInvalid = computed(() => $v.value.language.$errors.length > 0)
-
-async function save() {
-  Log.log(LogTag.LOGIC, 'Saving user info...')
-  $v.value.$touch()
-  if (validationFailed.value) {
-    Log.error(LogTag.LOGIC, 'User info is invalid')
-    return
-  }
-
-  await sendUserUpdateRequest(username.value, userEmail.value, language.value?.id)
-    .then((response) => {
-      authStore.setUser(response.data)
-      Log.log(LogTag.LOGIC, `User.id=${response.data.id} info saved`)
-    })
-    .catch((error) => {
-      Log.error(LogTag.LOGIC, 'Failed to save user info: ', error)
-      toaster.bakeError(userApiErrors.USER__UPDATING_FAILED, error.response?.data)
-    })
-}
-
-function logout() {
-  router.push({ name: routeNames.logout })
-}
-
-watch(username, () => {
-  if (usernameInvalid.value) {
-    $v.value.username.$reset()
-  }
-})
-
-watch(userEmail, () => {
-  if (userEmailInvalid.value) {
-    $v.value.userEmail.$reset()
-  }
-})
-
-watch(language, () => {
-  if (languageInvalid.value) {
-    $v.value.language.$reset()
-  }
-})
-
-onMounted(() => {
-  loadLanguageStore()
-    .then((success) => {
-      if (success) {
-        language.value = languageStore.getLanguage(user.value?.languageId ?? -1)
-      }
-    })
-})
-
 </script>
 
 <style scoped>
@@ -254,39 +79,6 @@ onMounted(() => {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-}
-
-.user-info {
-  flex: 0 0 auto;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: repeat(6, auto);
-  grid-auto-flow: column;
-  gap: 10px;
-  max-width: 600px;
-}
-
-@media (max-width: 600px) {
-  .user-info {
-    max-width: 100%;
-  }
-}
-
-.user-info-item {
-  align-items: center;
-}
-
-.user-info-icon {
-  --awesome-container--icon--color: var(--u-page--icon--color);
-}
-
-.user-controls {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-  margin-top: 20px;
 }
 
 </style>
