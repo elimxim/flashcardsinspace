6e1026adea8864e4a8b1906f74aae487a10a0c92
elimxim <elimxim@gmail.com>
2026-02-05 19:36:17 +0400

    DeferredLoading component instead of limited Suspense

---
+++ src/main/vue/src/components/DeferredLoading.vue
@@ -0,0 +1,56 @@
+<template>
+  <Transition name="fade" mode="out-in">
+    <div v-if="resolvedLoading" key="fallback">
+      <slot name="fallback"/>
+    </div>
+    <div v-else-if="!isDelaying" key="content">
+      <slot name="default"/>
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+import { useDeferredLoading } from '@/utils/deferred-loading.ts'
+import { onMounted } from 'vue'
+
+const props = withDefaults(defineProps<{
+  waitFor: () => Promise<void>
+  delayEntry?: number
+  minDuration?: number
+}>(), {
+  delayEntry: 200,
+  minDuration: 800,
+})
+
+const {
+  isDelaying,
+  resolvedLoading,
+  startLoading,
+  stopLoading
+} = useDeferredLoading({
+  delayEntry: props.delayEntry,
+  minDuration: props.minDuration
+})
+
+onMounted(async () => {
+  startLoading()
+  console.log('Waiting for promise...')
+  await props.waitFor()
+  console.log('Promise resolved')
+  await stopLoading()
+  console.log('Loading stopped')
+})
+
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
