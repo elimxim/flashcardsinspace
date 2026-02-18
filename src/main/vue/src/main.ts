d5a164e792091ec573820c7e94fd0939ca7fc67d
elimxim <elimxim@gmail.com>
2026-02-18 19:28:10 +0400

    avoid initial loaded blink, show it only when scripts loading time exceeds 200ms; and if it happened show the total visible time is 1200ms (with fade-in and fade-out)

--- src/main/vue/src/main.ts
+++ src/main/vue/src/main.ts
@@ -9,18 +9,50 @@ import { applyFaSolidIcons } from '@/fa-solid.ts'
 import { applyFaRegularIcons } from '@/fa-regular.ts'
 import { whoAmI } from '@/core-logic/user-logic.ts'
 import { PiniaResetPlugin } from '@/utils/pinia-reset-plugin.ts'
+import { Log, LogTag } from '@/utils/logger.ts'
 
-applyFaRegularIcons()
-applyFaSolidIcons()
+declare global {
+  interface Window {
+    loaderTimerId?: number
+    loaderAppearedAt?: number
+  }
+}
 
-const app = createApp(App)
-app.component('FontAwesomeIcon', FontAwesomeIcon)
+async function bootstrap() {
+  const loader = document.getElementById('initial-loader')
+  const loaderAppearedAt = window.loaderAppearedAt
 
-const pinia = createPinia()
-pinia.use(PiniaResetPlugin)
-app.use(pinia)
+  applyFaRegularIcons()
+  applyFaSolidIcons()
 
-whoAmI().then(() => {
-  app.use(router)
+  const app = createApp(App)
+  app.component('FontAwesomeIcon', FontAwesomeIcon)
+
+  const pinia = createPinia()
+  pinia.use(PiniaResetPlugin)
+  app.use(pinia)
+
+  try {
+    await whoAmI()
+    app.use(router)
+  } catch (error) {
+    Log.info(LogTag.SYSTEM, 'Could not determine user status', error)
+  }
+
+  if (loader && loaderAppearedAt) {
+    clearTimeout(window.loaderTimerId)
+    const loaderDuration = performance.now() - loaderAppearedAt
+    const remainingTime = Math.max(0, 1000 - loaderDuration)
+    // wait for at least 200ms (transition) + 800ms to avoid layout blink
+    await new Promise(resolve => setTimeout(resolve, remainingTime))
+    loader.classList.remove('visible')
+    // wait for 200ms (transition) before mounting the app
+    await new Promise(resolve => setTimeout(resolve, 200))
+  }
+
   app.mount('#app')
-})
+  Log.log(LogTag.SYSTEM, "App mounted to #app")
+  loader?.remove()
+}
+
+await bootstrap()
