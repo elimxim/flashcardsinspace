d65984ad9a82e6fd9a4fb1e1955280e2a293f42e
elimxim <elimxim@gmail.com>
2026-08-30 10:07:58 +0400

    Update versions, fix configs, define FontAwesomeIcon component to get seen by IDEA.

--- src/main/vue/env.d.ts
+++ src/main/vue/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+import type { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    FontAwesomeIcon: typeof FontAwesomeIcon
+  }
+}
+
+export {}
